3287419c83b8bc289f63a299d7bae0a51d770294
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-06-10 19:08:51 +0530

    feat: add FeaturedRestaurants component and restaurant data; update Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import FeaturedRestaurants from "../components/FeaturedRestaurants";
 
 function Home() {
     return (
         <>
             <Hero />
+            <FeaturedRestaurants />
         </>
     );
 }
