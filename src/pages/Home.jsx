02a8ad568840f8056b47ed93f9f803775f830495
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-06-12 10:16:48 +0530

    refactor: remove RestaurantSection component from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,11 @@ import FeaturedRestaurants from "../components/FeaturedRestaurants";
 import Statistics from "../components/Statistics";
 import Testimonials from "../components/Testimonials";
 import PartnerSection from "../components/PartnerSection";
-import RestaurantSection from "../components/RestaurantSection";
-
 
 function Home() {
     return (
         <>
             <Hero />
-            <RestaurantSection />
             <FeaturedRestaurants />
             <Statistics />
             <Testimonials/>
