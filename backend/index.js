cf0b091235f435e0b18165196671d47454a73a7a
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-06-23 19:17:42 +0530

    feat: initialize backend project with Express, Mongoose, and database connection configuration

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,13 @@ import express from "express";
 import AuthRouter from "./src/routers/auth.route.js";
 import PublicRouter from "./src/routers/public.route.js";
 
-
 const app = express();
+
+app.use(express.json());
+
 
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
-
-
 
 app.get("/", (req, res) => {
     console.log("Default Get API hit");
@@ -22,4 +22,5 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log("server started in port:", port);
-});  
+});
+
