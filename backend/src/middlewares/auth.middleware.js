f3d0a0cc2a4eb7e709e949cd4cecc35d936119c3
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-06-27 13:42:10 +0530

    add sample middleware functions for logging

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,7 @@
+export const sampleMiddleWare = async (req, res, next) => {
+    console.log("I am a middleware 1");
+};
+
+export const sampleMiddleWare2 = async (req, res, next) => {
+    console.log("I am a middleware 2");
+};
