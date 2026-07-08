2960a562d03054c7bb3a226f5b7c147baaf87ce4
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-07-08 20:11:21 +0530

    feat: implement database seeder script to initialize admin and user data

--- server/src/seeders/seed.js
+++ server/src/seeders/seed.js
@@ -2,15 +2,22 @@ import dotenv from "dotenv"
 dotenv.config();
 
 import dbConnection from "../config/dbConnection.config.js";
+import adminSeed from "./adminSeed.js";
+import userSeed from "./userSeed.js";
 
 const Seed = async () => {
     try {
-        dbConnection();
+        await dbConnection();
         console.log("Database connected successfully");
         
+        await adminSeed();
+        await userSeed();
+
+        console.log("All seeders executed successfully.");
+        process.exit(0);
     } catch (error) {
         console.error("Error connecting to database in seed script:", error);
-        throw error;
+        process.exit(1);
     }
 }
 
