b7fa288911c3d10b4d6319bf262fe633876af95f
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-07-17 19:01:20 +0530

    feat: add common routes for server status and health checks

--- server/src/routers/common.route.js
+++ server/src/routers/common.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { checkServerStatus } from "../controllers/common.controller.js";
+import { checkServerStatus, checkHealth } from "../controllers/common.controller.js";
 
 const router = express.Router();
 
 router.get("/", checkServerStatus);
+router.get("/health", checkHealth);
 
 export default router;
