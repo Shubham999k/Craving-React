a8ec2e82f8e9b9a64a596961ccbd44f5ec3aa21d
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-07-21 19:20:03 +0530

    feat: add express router for restaurant management endpoints

--- server/src/routers/restaurant.route.js
+++ server/src/routers/restaurant.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addMenuItem, getActiveOrders, updateOrderStatus } from "../controllers/restaurant.controller.js";
+import { addMenuItem, getActiveOrders, updateOrderStatus, registerRestaurant } from "../controllers/restaurant.controller.js";
 
 const router = express.Router();
 
+router.post("/register", registerRestaurant);
 router.post("/menu", addMenuItem);
 router.get("/orders", getActiveOrders);
 router.put("/orders/:orderId/status", updateOrderStatus);
